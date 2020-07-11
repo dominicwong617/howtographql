e4b55a53014c872f54632b3b6da38e30dd2560f7
Dominic Wong <dominic.wong.617@gmail.com>
2020-07-11 13:56:00 +0800

    Simple mutation

---
+++ hackernews-node/src/index.js
@@ -0,0 +1,43 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (parent, args) => links.find((l) => l.id === args.id)
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (parent, args) => {
+      let link = links.find((l) => l.id === args.id)
+      link.description = args.description
+      link.url = args.url
+      return link
+    },
+    deleteLink: (parent, args) => {
+      links = links.filter((link) => link.id !== args.id)
+    },
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
