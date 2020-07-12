fc4ef01298449a6a56ecf56699f576d7e74400fc
Dominic Wong <dominic.wong.617@gmail.com>
2020-07-12 18:29:39 +0800

    Queries: Loading Links

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
@@ -21,11 +22,7 @@ const client = new ApolloClient({
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
-  <ApolloProvider>
+  </ApolloProvider>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+)
 serviceWorker.unregister();
